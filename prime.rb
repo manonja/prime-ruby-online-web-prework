d5ae71b8083fbd7bc180a51833e00312ac2dc646
manon jacquin <manon.jacquin@gmail.com>
2018-10-05 17:54:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,8 @@
 # Add  code here!
-# def prime?(number)
-#   counter = 2
-#   while counter < number
-#     if number % counter == 0 
-#       return false
-#     end
-#   end
-# return true
-# end
-
 
 def prime?(num)
   n = 2
-  while n < num && n > 0
+  while n < num && num > 0
     return false if num % n == 0 
     n += 1
   end
