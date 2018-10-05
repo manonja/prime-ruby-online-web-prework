502ee303b6e0a5a3d16c1393e35b6990a272c693
manon jacquin <manon.jacquin@gmail.com>
2018-10-05 17:47:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,21 @@
 # Add  code here!
-def prime?(number)
-  counter = 2
-  while counter < number
-    if number % counter == 0 
-      return false
-    end
+# def prime?(number)
+#   counter = 2
+#   while counter < number
+#     if number % counter == 0 
+#       return false
+#     end
+#   end
+# return true
+# end
+
+
+def prime? n
+  for d in 2..(n - 1)
+   if (n % d) == 0
+    return false
+   end
   end
-return true
-end
 
-
+  true
+ end
